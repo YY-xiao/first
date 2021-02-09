a5065006471a5ef1655e72dd257ee9d356fbcf0c
YY-xiao <18091289607@163.com>
2021-02-10 01:14:27 +0800

    试一试clone

--- main.py
+++ main.py
@@ -3,15 +3,19 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import xlrd
+import xlwt
+data =xlrd.open_workbook(r'F:\XXXYAN\pythonProject\milk\12.xls')
+table = data.sheet_by_name(sheet_name='12')
+wbk = xlwt.Workbook(r'F:\XXXYAN\pythonProject\milk\12.xls')
+#wbk.save('12.xls')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
+print(table.name)
+print(table.nrows)
+print(table.ncols)
+rows = table.row_values(3)#输出第三行的全部值，赋值给rows，这是以数组的形式
+cols = table.col_values(2)#
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    print_hi('hhh')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a=table.row(1)[0].value.encode('utf-8')#获取指定某单元格内容

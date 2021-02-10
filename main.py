20029ca294a1b8be86994522e478fc89d46655c5
YY-xiao <18091289607@163.com>
2021-02-10 11:36:00 +0800

    比较两张excel表中某两列的值，输出不一样的行的序号

--- main.py
+++ main.py
@@ -5,17 +5,31 @@
 
 import xlrd
 import xlwt
-data =xlrd.open_workbook(r'F:\XXXYAN\pythonProject\milk\12.xls')
-table = data.sheet_by_name(sheet_name='12')
-wbk = xlwt.Workbook(r'F:\XXXYAN\pythonProject\milk\12.xls')
+data1 =xlrd.open_workbook(r'F:\XXXYAN\pythonProject\milk\21.xls')
+table1 = data1.sheet_by_name(sheet_name='12')
+data2 =xlrd.open_workbook(r'F:\XXXYAN\pythonProject\milk\22.xls')
+table2 = data2.sheet_by_name(sheet_name='Sheet3')
+wbk = xlwt.Workbook(r'F:\XXXYAN\pythonProject\milk\11.xls')
 #wbk.save('12.xls')
+rows1=table1.nrows
+rows2=table2.nrows
+cols1=table1.ncols
+cols2=table2.ncols
 
-print(table.name)
-print(table.nrows)
-print(table.ncols)
-rows = table.row_values(3)#输出第三行的全部值，赋值给rows，这是以数组的形式
-cols = table.col_values(2)#
+for row in range(2,rows1-1):
+    cell1 = table1.cell(row, 2)
+    cell2=table2.cell(row,1)
+    vcell1 = cell1.value
+    vcell2=cell2.value
+    if(vcell1!=vcell2):
+        print(row+1)
+
+print(table1.name)
+print(table1.nrows)
+print(table1.ncols)
+rows = table1.row_values(3)#输出第三行的全部值，赋值给rows，这是以数组的形式
+cols = table1.col_values(2)#
 # Press the green button in the gutter to run the script.
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-a=table.row(1)[0].value.encode('utf-8')#获取指定某单元格内容
+a=table1.row(1)[0].value.encode('utf-8')#获取指定某单元格内容
